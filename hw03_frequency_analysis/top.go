4e2d1eccc54a4423b61d2a6dec6992925f19beb1
mike080 <mike080@example.com>
2022-06-22 07:39:41 +0300

    HW3 for review - 2

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -3,6 +3,7 @@ package hw03frequencyanalysis
 import (
 	"regexp"
 	"sort"
+	"strings"
 )
 
 type Words struct {
@@ -10,29 +11,50 @@ type Words struct {
 	Count int
 }
 
-func Top10(s string) []string {
+var t1 = regexp.MustCompile(`[^\wа-яА-Я-]`)
+
+var t2 = regexp.MustCompile(`\s`)
+
+func Top10(s string, taskWithAsteriskIsCompleted bool) []string {
 	counts := make(map[string]int)
-	t := regexp.MustCompile(`\s+`)
-	v := t.Split(s, -1)
-	for _, sl := range v {
-		if sl != " " {
-			counts[sl]++
+
+	v1 := t1.Split(s, -1)
+	v2 := t2.Split(s, -1)
+	if taskWithAsteriskIsCompleted {
+		for _, sl := range v1 {
+			if sl != "" && sl != "-" {
+				counts[strings.ToLower(sl)]++
+			}
+		}
+	} else {
+		for _, sl := range v2 {
+			if sl != "" {
+				counts[sl]++
+			}
 		}
 	}
-	words := []Words{}
+
+	words := make([]Words, 0, len(counts))
 	for k, v := range counts {
 		words = append(words, Words{k, v})
 	}
-	sort.SliceStable(words, func(i, j int) bool { return words[i].Word < words[j].Word })
-	sort.SliceStable(words, func(i, j int) bool { return words[i].Count > words[j].Count })
-	var out []string
-	for i, w := range words {
-		if w.Count > 1 {
-			out = append(out, w.Word)
+
+	sort.SliceStable(words, func(i, j int) bool {
+		if words[i].Count > words[j].Count {
+			return true
 		}
-		if i == 9 {
+		if words[i].Count < words[j].Count {
+			return false
+		}
+		return words[i].Word < words[j].Word
+	})
+
+	out := make([]string, 0, 10)
+	for i, w := range words {
+		if i == 10 {
 			break
 		}
+		out = append(out, w.Word)
 	}
 	return out
 }
